b879d4eb270966aece4ba5aa5dc30ccd308be131
Zulfa Nurfajar <znurfajar@gmail.com>
2023-01-21 01:53:48 +0700

    add new changes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+let logo = document.querySelector("#home")
+logo.style.fontSize = "2rem"
 
-
+let enroll = document.querySelector("#enroll")
+enroll.style.fontSize = "2rem"
 
 //build side menu
-let sideBar = document.querySelector("#menuOpen");
+let sideBar = document.querySelector(".sideMenu");
 let openMenu = document.querySelector(".hamburger");
-let closedMenu = document.querySelector(".closedTag");
+let closedMenu = document.querySelector(".closed");
 
 openMenu.onclick = function(){
     sideBar.classList.add('active');
